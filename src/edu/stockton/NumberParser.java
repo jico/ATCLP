3bcda9258bba2e26dcb4348a45a087f1cf3e29ae
Jico Baligod <jicobaligod@gmail.com>
2011-03-03 16:51:11 -0500

    removed whitespace chars from numbers.xml to avoid #text nodes, moved nicely formatted xml to numbersPretty.xml

--- src/edu/stockton/NumberParser.java
+++ src/edu/stockton/NumberParser.java
@@ -15,61 +15,86 @@ public class NumberParser {
 	private static DocumentBuilder loader;
 	private static Document numbersDoc;
 	private static Element tree;
+	private static NodeList numbers;
+	private static String xmlFilename = "numbers.xml";
 	
 	public NumberParser() throws Exception {
 		loader = factory.newDocumentBuilder();
+		numbersDoc = loader.parse(xmlFilename);
+		tree = numbersDoc.getDocumentElement();
+		numbers = tree.getChildNodes();
+		
+		// Strip #text nodes
+		/*
+		for(int i = 0; i < numbers.getLength(); i++) {
+			NodeList numberChildren = numbers.item(i).getChildNodes();
+			
+			if(numbers.item(i).getNodeName() == "#text") {
+				Node rem = numbers.item(i);
+				tree.removeChild(rem);
+			}
+		}
+		*/
+		
+		for(int i = 0; i < numbers.getLength(); i++) {
+			System.out.println(numbers.item(i));
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
-		try {
-		      // first of all we request out 
-		      // DOM-implementation through factory above
-		      // then we have to create document-loader:
-		      DocumentBuilder loader = factory.newDocumentBuilder();
-
-		      // loading a DOM-tree...
-		      Document document = loader.parse("src/numbers.xml");
-		      // at last, we get a root element:
-		      Element tree = document.getDocumentElement();
-		      System.out.println(tree);
-		      System.out.println("root has children: " + tree.hasChildNodes());
+		NumberParser numParser = new NumberParser();
+		
+		String testCase = "five";
+		if(numParser.isNumber(testCase)) {
+			System.out.println(testCase + " is a number");
+		} else System.out.println(testCase + " is not a number");
+		
+		/*
+	      // first of all we request out 
+	      // DOM-implementation through factory above
+	      // then we have to create document-loader:
+	      loader = factory.newDocumentBuilder();
+	
+	      // loading a DOM-tree...
+	      Document document = loader.parse(xmlFilename);
+	      // at last, we get a root element:
+	      tree = document.getDocumentElement();
+	      System.out.println(tree);
+	      System.out.println("root has children: " + tree.hasChildNodes());
+	      
+	      NodeList nodes = tree.getChildNodes();
+	      int l = nodes.getLength();
+	      System.out.println("Number of children: " + l);
+	      for(int i = 0; i < l; i++) {
+	    	  Node node = nodes.item(i);
+	    	  if(node.getNodeName() == "number") {
+	    		  Element number = (Element) node;
+	    		  NodeList numNodes = number.getChildNodes();
+	    		  for(int j = 0; j < numNodes.getLength(); j++) {
+	    			  if(numNodes.item(j).getNodeName() == "text") {
+	    				  Element numVal = (Element) numNodes.item(j);
+	    				  System.out.println(numVal.getTextContent());
+	    			  }
+	    		  }
+	    	  }
+	      }
+	      */
 		      
-		      NodeList nodes = tree.getChildNodes();
-		      int l = nodes.getLength();
-		      System.out.println("Number of children: " + l);
-		      for(int i = 0; i < l; i++) {
-		    	  Node node = nodes.item(i);
-		    	  if(node.getNodeName() == "number") {
-		    		  Element number = (Element) node;
-		    		  NodeList numNodes = number.getChildNodes();
-		    		  for(int j = 0; j < numNodes.getLength(); j++) {
-		    			  if(numNodes.item(j).getNodeName() == "text") {
-		    				  Element numVal = (Element) numNodes.item(j);
-		    				  System.out.println(numVal.getTextContent());
-		    			  }
-		    		  }
-		    	  }
-		    	  
-		      }
-		      
-		    } catch (IOException ex) {
-		      // any IO errors occur:
-		      handleError(ex);
-		    } catch (SAXException ex) {
-		      // parse errors occur:
-		      handleError(ex);
-		    } catch (ParserConfigurationException ex) {
-		      // document-loader cannot be created which,
-		      // satisfies the configuration requested
-		      handleError(ex);
-		    } catch (FactoryConfigurationError ex) {
-		      // DOM-implementation is not available 
-		      // or cannot be instantiated:
-		      handleError(ex);
-		    }
 	}
 	
-	private static final void handleError(Throwable ex) {
-	    // ... handle error here...
-	  }
+	/**
+	 * Checks to see if a word represents a number.
+	 * @param s The string of text
+	 * @return True if the word represents a number, false otherwise
+	 */
+	public static boolean isNumber(String s) {
+		for(int i = 0; i < numbers.getLength(); i++) {
+			Element number = (Element) numbers.item(i);
+			if(s.equals(number.getFirstChild().getTextContent())) return true;
+		}
+		
+		return false;
+	}
+	
+	
 }
