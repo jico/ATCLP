915d46d1f40affbad6a80d5b80bd601bd3edd4dd
Jico Baligod <jicobaligod@gmail.com>
2011-03-28 04:20:10 -0400

    param get/set methods

--- src/edu/stockton/Param.java
+++ src/edu/stockton/Param.java
@@ -5,6 +5,34 @@ public class Param {
 	private String inputRegex;
 	private String outputRegex;
 	
+	public Param(String inputRegex, String outputRegex, String type) {
+		this.inputRegex = inputRegex;
+		this.outputRegex = outputRegex;
+		this.type = type;
+	}
 	
+	public String getType() {
+		return type;
+	}
+	
+	public String getInputRegex() {
+		return inputRegex;
+	}
+	
+	public String getOutputRegex() {
+		return outputRegex;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setInputRegex(String inputRegex) {
+		this.inputRegex = inputRegex;
+	}
+	
+	public void setOutputRegex(String outputRegex) {
+		this.outputRegex = outputRegex;
+	}
 	
 }
