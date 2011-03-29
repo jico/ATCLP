011461253fe8c8df3b495a250a14db7935c26c4f
Jico Baligod <jicobaligod@gmail.com>
2011-03-28 22:52:06 -0400

    added javadoc to methods and class description

--- src/edu/stockton/Callsign.java
+++ src/edu/stockton/Callsign.java
@@ -1,13 +1,15 @@
 package edu.stockton;
 
+/**
+ * The Callsign data object contains information for a 
+ * particular airline. 
+ * Used primarily for the CallsignEngine class.
+ *
+ */
 public class Callsign {
 	private String designator;
 	private String company;
 	private String telephony;
-	
-	public Callsign() {
-		
-	}
 	
 	public Callsign(String telephony, String designator, String company) {
 		this.telephony = telephony;
@@ -15,26 +17,50 @@ public class Callsign {
 		this.company = company;
 	}
 	
+	/**
+	 * Gets the 3-letter designator
+	 * @return The designator
+	 */
 	public String getDesignator() {
 		return designator;
 	}
 	
+	/**
+	 * Gets the aircraft company
+	 * @return The company
+	 */
 	public String getCompany() {
 		return company;
 	}
 	
+	/**
+	 * Gets the telephony 
+	 * @return The telephony
+	 */
 	public String getTelephony() {
 		return telephony;
 	}
 	
+	/**
+	 * Sets the designator value
+	 * @param designator The 3-letter designator
+	 */
 	public void setDesignator(String designator) {
 		this.designator = designator;
 	}
 	
+	/**
+	 * Sets the company value
+	 * @param company The aircraft company
+	 */
 	public void setCompany(String company) {
 		this.company = company;
 	}
 	
+	/**
+	 * Sets the telephony value
+	 * @param telephony The telephony string
+	 */
 	public void setTelephony(String telephony) {
 		this.telephony = telephony;
 	}
