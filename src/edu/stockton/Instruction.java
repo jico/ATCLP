70b6cc456fe926f7ff3c6367280ab0ae4dafaca8
Jico Baligod <jicobaligod@gmail.com>
2011-03-28 23:11:57 -0400

    added class description and javadoc

--- src/edu/stockton/Instruction.java
+++ src/edu/stockton/Instruction.java
@@ -2,14 +2,20 @@ package edu.stockton;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a defined and valid ATC instruction, corresponding
+ * to an XML node in the instructions.xml data library.
+ * Contains the phrase regular expression of the instruction, type,
+ * and a list of the different parameters it may have.
+ * 
+ * Used only with the InstructionEngine.
+ *
+ */
 public class Instruction {
 	private String phrase;
 	private String type;
 	private ArrayList<Param> params;
 	
-	public Instruction() {
-		
-	}
 	
 	public Instruction(String phrase, String type, ArrayList<Param> params) {
 		this.phrase = phrase;
@@ -17,14 +23,26 @@ public class Instruction {
 		this.params = params;
 	}
 	
+	/**
+	 * Gets the phrase of the instruction
+	 * @return The instruction phrase
+	 */
 	public String getPhrase() {
 		return phrase;
 	}
 	
+	/**
+	 * Gets the type of the instruction
+	 * @return The instruction type
+	 */
 	public String getType() {
 		return type;
 	}
 	
+	/**
+	 * Gets the list of instruction parameters
+	 * @return ArrayList of Param objects representing the instruction parameters
+	 */
 	public ArrayList<Param> getParams() {
 		return params;
 	}
