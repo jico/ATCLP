e6ea347142dfe4010b814cc77342b8b069fba1d2
Jico Baligod <jicobaligod@gmail.com>
2011-03-28 22:48:35 -0400

    added javadoc to methods

--- src/edu/stockton/ATCCommand.java
+++ src/edu/stockton/ATCCommand.java
@@ -1,13 +1,12 @@
 package edu.stockton;
 
-// Mark Mucciarone
-// Last Date worked on: 3/24/2011
-//
-// Typical get and set methods to access/modify private 
-// instance fields (recipient, type, parameter). i.e. 
-// getRecipient() returns recipient instance field
-// along with toXML command to output all fields into an XML
-// ATCCommand line.  
+/**
+ * ATCCommand data object holds the processed command
+ * values and contains methods to return them.
+ *   
+ * @author Mark Mucciarone
+ *
+ */
 public class ATCCommand {
 	private String recipient;
 	private String type;
@@ -17,41 +16,64 @@ public class ATCCommand {
 		this.recipient = recipient;		
 		this.type = type;		
 		this.parameter = parameter;	
-		}	
+	}	
 	
+	/**
+	 * Gets the recipient value
+	 * @return The recipient string
+	 */
 	public String getRecipient() {		
 		return recipient;	
-		}
+	}
 	
+	/**
+	 * Gets the type
+	 * @return The type string
+	 */
 	public String getType() {		
 		return type;	
-		}
+	}
 	
+	/**
+	 * Gets the parameter
+	 * @return The parameter string
+	 */
 	public String getParameter() {		
 		return parameter;	
-		}
+	}
 	
+	/**
+	 * Sets the recipient value
+	 * @param recipient The recipient
+	 */
 	public void setRecipient(String recipient) {
 		this.recipient = recipient;
 	}
 	
+	/**
+	 * Sets the type value
+	 * @param type The type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 	
+	/**
+	 * Sets the parameter value
+	 * @param parameter The parameter
+	 */
 	public void setParameter(String parameter) {
 		this.parameter = parameter;
 	}
 	
+	/**
+	 * Outputs the data in an XML string
+	 * @return The recipient, type, and parameter values in XML format
+	 */
 	public String toXML(){
-		
 		String temp = "<ATCCommand><recipient>" + recipient + "</recipient><type>" + type + "</type><parameter>" + parameter + "</parameter></ATCCommand>";
 		return temp;
 	}
-	
-	
-	
-	
 	
 }
 
